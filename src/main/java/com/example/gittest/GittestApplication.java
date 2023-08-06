87eaafe5e8b7db30d0a2977504b87baf539c2da1
SergeiStepanov731 <iffymailone@gmail.com>
2023-08-06 15:05:41 +0300

    rollback

--- src/main/java/com/example/gittest/GittestApplication.java
+++ src/main/java/com/example/gittest/GittestApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GittestApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(GittestApplication.class, args);
     }
 
